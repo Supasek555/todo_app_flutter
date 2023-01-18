a2b5e776374ab0a43ea2089a891ea8616dacd0be
Supasek Dhanabordeephat <Supasekmai@gmail.com>
2023-01-18 14:53:14 +0700

    Search Bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,29 +8,65 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
-      appBar: AppBar(
-        title:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Icon(
-            Icons.menu,
-            color: tdBlack,
-            size: 30,
-          ),
-          Container(
-            height: 40,
-            width: 40,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/images/bong.png'),
-            ),
-          )
-        ]),
-        backgroundColor: tdBGColor,
-        elevation: 0,
-      ),
-      body: Center(
-        child: Text("Jai Hind"),
+      appBar: _buildAppBar(),
+      body: Container(
+        color: tdRed,
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
       ),
     );
   }
+}
+
+AppBar _buildAppBar() {
+  return AppBar(
+    title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Icon(
+        Icons.menu,
+        color: tdBlack,
+        size: 30,
+      ),
+      Container(
+        height: 40,
+        width: 40,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset('assets/images/bong.png'),
+        ),
+      )
+    ]),
+    backgroundColor: tdBGColor,
+    elevation: 0,
+  );
+}
+
+Widget searchBox() {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 15),
+    padding: EdgeInsets.symmetric(horizontal: 5),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: const TextField(
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(5),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey)),
+    ),
+  );
 }
